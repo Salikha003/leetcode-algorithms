a9579c38c384fc718a64e6d74b9eac19d50db790
Salikha <solihahusniddinova27@gmail.com>
2025-03-23 09:24:45 +0500

    Completed

--- 3242-count-elements-with-maximum-frequency/2024-03-08 12.18.45 - Accepted - runtime 38ms - memory 16.6MB.py
+++ 3242-count-elements-with-maximum-frequency/2024-03-08 12.18.45 - Accepted - runtime 38ms - memory 16.6MB.py
@@ -3,10 +3,3 @@ class Solution:
         c = Counter(nums)
         m = c.most_common(1)[0][1]
         return sum(v for k,v in c.items() if v == m)
-    
-
-class Solutions:
-    def maxFrequencyElements(self, nums: List[int]) -> int:
-        c = Counter(nums)
-        m = c.most_common(1)[0][1]
-        return sum(v for k,v in c.items() if v == m)
