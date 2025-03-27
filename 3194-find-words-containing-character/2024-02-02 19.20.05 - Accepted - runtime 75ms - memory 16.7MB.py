8b511a7799d15b4b779f0268481a65ffa794f954
Salikha <solihahusniddinova27@gmail.com>
2025-03-27 22:49:29 +0500

    fixed44

--- 3194-find-words-containing-character/2024-02-02 19.20.05 - Accepted - runtime 75ms - memory 16.7MB.py
+++ 3194-find-words-containing-character/2024-02-02 19.20.05 - Accepted - runtime 75ms - memory 16.7MB.py
@@ -1,3 +1,8 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        return [f for f in range(len(words)) if x in words[f]]
+
+
 class Solutions:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         return [f for f in range(len(words)) if x in words[f]]
