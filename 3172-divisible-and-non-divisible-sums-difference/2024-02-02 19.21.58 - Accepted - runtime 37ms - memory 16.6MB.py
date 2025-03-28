2afd7bf1b49a0688b2436090c743c80a0fb8f6d7
Salikha <solihahusniddinova27@gmail.com>
2025-03-28 23:10:35 +0500

    fixed6

--- 3172-divisible-and-non-divisible-sums-difference/2024-02-02 19.21.58 - Accepted - runtime 37ms - memory 16.6MB.py
+++ 3172-divisible-and-non-divisible-sums-difference/2024-02-02 19.21.58 - Accepted - runtime 37ms - memory 16.6MB.py
@@ -1,3 +1,14 @@
+class Solutions:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        a = b = 0
+        for i in range(1, n + 1):
+            if i % m:
+                a += i
+            else:
+                b += i
+        
+        return a - b
+    
 class Solutionbbv:
     def differenceOfSums(self, n: int, m: int) -> int:
         a = b = 0
