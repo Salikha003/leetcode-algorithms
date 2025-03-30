cc020e6f7185c4223c6c124b0c1117aac4447fcb
Salikha <solihahusniddinova27@gmail.com>
2025-03-30 15:04:22 +0500

    completed

--- 3067-modify-columns/2024-04-01 14.35.25 - Accepted - runtime 776ms - memory 65.5MB.pd.py
+++ 3067-modify-columns/2024-04-01 14.35.25 - Accepted - runtime 776ms - memory 65.5MB.pd.py
@@ -3,10 +3,3 @@ import pandas as pd
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
     employees.salary *= 2
     return employees
-
-
-def modifySalaryColumns(employees: pd.DataFrame) -> pd.DataFrame:
-    employees.salary *= 2
-    return employees
-
-
