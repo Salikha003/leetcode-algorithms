d1c2a1b18fe47cec31cd37be8b0ff0d95f882262
Salikha <solihahusniddinova27@gmail.com>
2025-03-31 17:20:23 +0500

    fixed98

---
+++ 3066-create-a-new-column/2024-04-01 14.38.48 - Accepted - runtime 719ms - memory 65.3MB.pd.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees.salary * 2
+    return employees
+
+
+def createBonusColumnasasas(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees.salary * 2
+    return employees
