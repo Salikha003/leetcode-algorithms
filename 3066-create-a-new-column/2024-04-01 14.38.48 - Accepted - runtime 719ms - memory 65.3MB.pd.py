8c6231c9b5e3a31bce711ab7c33531fd7a138017
Salikha <solihahusniddinova27@gmail.com>
2025-03-31 17:23:55 +0500

    completed

--- 3066-create-a-new-column/2024-04-01 14.38.48 - Accepted - runtime 719ms - memory 65.3MB.pd.py
+++ 3066-create-a-new-column/2024-04-01 14.38.48 - Accepted - runtime 719ms - memory 65.3MB.pd.py
@@ -3,9 +3,3 @@ import pandas as pd
 def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
     employees['bonus'] = employees.salary * 2
     return employees
-
-
-def createBonusColumnasasas(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['bonus'] = employees.salary * 2
-    return employees
-
