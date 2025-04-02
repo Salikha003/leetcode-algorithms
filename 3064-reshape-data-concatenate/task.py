819efea181cae0569da2d4fec33600a33134ead6
Salikha <solihahusniddinova27@gmail.com>
2025-04-02 20:22:14 +0500

    fixed91

--- 3064-reshape-data-concatenate/task.py
+++ 3064-reshape-data-concatenate/task.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
     return employees.head(3)
+
+
+import pandas as pd
+
+def selectSecondRows(employees: pd.DataFrame) -> pd.DataFrame:
+    return employees.head(3)
