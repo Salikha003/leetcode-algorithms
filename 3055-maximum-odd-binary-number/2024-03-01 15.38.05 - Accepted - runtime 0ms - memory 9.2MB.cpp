73bd604aa902bd2f2801ced1f4863480cdafa950
Salikha <solihahusniddinova27@gmail.com>
2025-04-04 23:01:27 +0500

    fixed8

--- 3055-maximum-odd-binary-number/2024-03-01 15.38.05 - Accepted - runtime 0ms - memory 9.2MB.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    string maximumOddBinaryNumber(string s) {
-        int o = count(s.begin(), s.end(), '1');
-        int z = s.length() - o;
-
-        return string(o - 1, '1') + string(z, '0') + "1";
-    }
-};
