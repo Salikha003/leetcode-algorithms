4d1cf446e7509c5e704efd4c7c0501cf8c7e57c9
Salikha <solihahusniddinova27@gmail.com>
2025-04-20 20:31:38 +0500

    fixed98

--- 2573-remove-nodes-from-linked-list/2024-02-25 19.28.14 - Accepted - runtime 258ms - memory 164.4MB.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    ListNode* removeNodes(ListNode* head) {
-        if (head == NULL) return head;
-        head -> next = removeNodes(head -> next);
-        return head->next && head->next->val > head->val? head->next: head;
-    }
-};
