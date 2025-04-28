2a01a607d74745822201f74c487c0b73cb500b13
Salikha <solihahusniddinova27@gmail.com>
2025-04-28 23:26:16 +0500

    fixed91

--- 2502-sort-the-people/task.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        return [name for name,height in sorted(zip(names, heights), key=lambda x:-x[1])]
