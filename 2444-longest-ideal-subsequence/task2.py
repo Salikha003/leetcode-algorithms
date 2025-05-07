d75ddb58fe3d6c16a394f4a8413774d206f2e67e
Salikha <solihahusniddinova27@gmail.com>
2025-05-07 21:35:21 +0500

    fixed93

--- 2444-longest-ideal-subsequence/task2.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def longestIdealString(self, s: str, k: int) -> int:
-        dp = [0] * 128
-        for i in map(ord, s):
-            dp[i] = max(dp[i - k: i + k + 1]) + 1
-        
-        return max(dp)
