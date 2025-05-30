fcc0fe81f7e703eea086f6d085b5c7c73965b101
Salikha <solihahusniddinova27@gmail.com>
2025-05-30 23:19:55 +0500

    fixed4

--- 2331-intersection-of-multiple-arrays/taskk1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def intersection(self, nums: List[List[int]]) -> List[int]:
-        l = list(map(set, nums))
-        t = l[0]
-        for i in l[1:]:
-            t = t.intersection(i)
-        return sorted(t)
-        
